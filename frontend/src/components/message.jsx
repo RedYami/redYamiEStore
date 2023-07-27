86efeb15feb83621632e9237eea1050a13f96f49
hyena <mesutkee@gmail.com>
2023-07-27 09:03:34 +0630

    added new h5 and some style changes in message.jsx

--- frontend/src/components/message.jsx
+++ frontend/src/components/message.jsx
@@ -11,32 +11,37 @@ export default function MessageBox({ onSendMessage, user }) {
   const [title, setTitle] = useState("");
   return (
     <>
-      <div className="socialMedia row  rounded p-2 shadow">
-        <a
-          href="https://www.facebook.com/profile.php?id=100089539521655"
-          className=" col d-flex justify-content-evenly"
-          style={{ textDecoration: "none" }}
-          target="_blank"
-        >
-          <span style={{ color: "black" }}>contact us on</span>
-          <i
-            className="fa fa-facebook-f"
-            style={{
-              fontSize: "30px",
-              color: "blue",
-            }}
-          ></i>
-        </a>
-        <div className=" col d-flex justify-content-evenly">
-          <span>See us on</span>
+      <div
+        className="messageHead rounded p-2 shadow"
+        style={{ maxWidth: "500px", margin: "auto" }}
+      >
+        <h5 className="text-center">YOU CAN FIND US ON</h5>
+        <hr />
+        <div className="socialMedia">
+          <a
+            href="https://www.facebook.com/profile.php?id=100089539521655"
+            className="  justify-content-center"
+            style={{ textDecoration: "none" }}
+            target="_blank"
+          >
+            <i
+              className="fa fa-facebook-f"
+              style={{
+                fontSize: "30px",
+                color: "blue",
+              }}
+            ></i>
+          </a>
           <i
             className="fa fa-twitter"
             style={{ fontSize: "30px", color: "aqua" }}
           ></i>
+
+          <i class="fa fa-instagram" style={{ fontSize: "30px" }}></i>
         </div>
       </div>
       <div className="messageBox shadow rounded p-3 mt-3">
-        <h5>- OR MESSAGE US HERE -</h5>
+        <h5> OR MESSAGE US HERE </h5>
         <hr />
         <div>
           <label>Title</label>
